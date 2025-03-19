14f2866f18393a87c1887998167f6c9321760283
jxxngsxhx <jeongsuho0401@gmail.com>
2025-03-19 19:33:32 +0900

    Initial Commit

---
+++ FreeMates/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FreeMates
+//
+//  Created by 정수호 on 3/19/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FreeMates/FreeMatesApp.swift
@@ -0,0 +1,17 @@
+//
+//  FreeMatesApp.swift
+//  FreeMates
+//
+//  Created by 정수호 on 3/19/25.
+//
+
+import SwiftUI
+
+@main
+struct FreeMatesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
