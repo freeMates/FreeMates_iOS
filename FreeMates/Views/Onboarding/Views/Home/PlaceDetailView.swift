0a2d12206a19a621b508e4fce4e3139e1409aa75
jxxngsxhx <jeongsuho0401@gmail.com>
2025-04-11 20:38:03 +0900

    feat

---
+++ FreeMates/Views/Onboarding/Views/Home/PlaceDetailView.swift
@@ -0,0 +1,18 @@
+//
+//  PlaceDetailView.swift
+//  FreeMates
+//
+//  Created by 정수호 on 4/11/25.
+//
+
+import SwiftUI
+
+struct PlaceDetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PlaceDetailView()
+}
